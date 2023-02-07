e46c052ecd649564cbc77523d3578cd329525c69
jonathan <jonathan.dupau24@gmail.com>
2023-02-07 09:52:00 +0100

    fix the loop to get all followers

--- DATA/SCRAPPER/call_api_service.py
+++ DATA/SCRAPPER/call_api_service.py
@@ -23,19 +23,39 @@ class Call_api_service:
 
     # get all the followers of the streamer
     def get_user_follows(self, id_twitch):
-        endpoint = 'https://api.twitch.tv/helix/users/follows?to_id=' + id_twitch
+        endpoint = 'https://api.twitch.tv/helix/users/follows?to_id=' + id_twitch + '&first=100'
 
         response = requests.get(endpoint, headers=self.head).json()
 
-        return response
+        # total_followers = response['total']
+        followers_data = response['data']
+        cursor = response['pagination']['cursor']
 
-    def pagination(self, id_twitch, cursor):
-        endpoint = 'https://api.twitch.tv/helix/users/follows?to_id=' + id_twitch + '&after=' + cursor
+        followers = {}
+        for follower in followers_data:
+            follower_pseudo = follower['from_name']
+            follower_twitch_id = follower['from_id']
+            followers[follower_pseudo] = follower_twitch_id
 
-        response = requests.get(endpoint, headers=self.head).json()
+        while(cursor):
+            endpoint_pagination = 'https://api.twitch.tv/helix/users/follows?to_id=' + id_twitch + '&first=100' + '&after=' + cursor
 
-        return response
+            response_pagination = requests.get(endpoint_pagination, headers=self.head).json()
 
+            followers_data_pagination = response_pagination['data']
+
+            for follower_pagination in followers_data_pagination:
+                follower_pseudo_pagination = follower_pagination['from_name']
+                follower_twitch_id_pagination = follower_pagination['from_id']
+                followers[follower_pseudo_pagination] = follower_twitch_id_pagination
+
+            if 'cursor' in response_pagination['pagination']:
+                cursor = response_pagination['pagination']['cursor']
+
+            else:
+                return followers
+
+        return followers
 
     # get all streams on twitch RIGHT NOW
     def get_streams(self):
@@ -69,3 +89,23 @@ class Call_api_service:
         if response['data']:
             return response['data']
         return 'problem with username'
+
+
+
+import authenticator
+import twitch_api_auth
+
+client_id = twitch_api_auth.Client_ID
+secret = twitch_api_auth.Secret
+
+auth = authenticator.Auth(client_id, secret)
+access_token = auth.get_auth()
+
+ca = Call_api_service(client_id, access_token)
+
+dev_optic_id = ca.get_user_id('Squeezie')
+
+followers = ca.get_user_follows(str(dev_optic_id))
+
+print(followers)
+print(len(followers))
